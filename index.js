ce374fcf9819c2409440610328fa9954839e8d01
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-05-01 16:25:52 -0400

    fixed ability to update color array dynamically

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-let colorsArr = []
 const colorInput = document.getElementById('color-input')
 const schemeInput = document.getElementById('choose-scheme')
 
-document.getElementById('inputs-form').addEventListener("submit", getColorScheme)
+document.getElementById('inputs-form').addEventListener("submit", (e)=> {
+  e.preventDefault()
+  let colorValue = colorInput.value.slice(1)
+  let colorScheme = schemeInput.value
+  getColorScheme(colorValue, colorScheme)
+})
 
-function getColorScheme(e){
-    e.preventDefault()
-    let color = colorInput.value.slice(1)
-    let colorScheme = schemeInput.value
-    
-    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${colorScheme}&count=5`)
+function getColorScheme(color, scheme){
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${scheme}&count=5`)
       .then(res => res.json())
       .then( data => {
-            const hexArray = data.colors
-            hexArray.map(color => {
+            let colorsArr = []
+            data.colors.map(color => {
                 colorsArr.push(color.hex.value)
-                colorsArr.push(color.hex.value)
+                return colorsArr
           })
-          renderColors()
+          renderColors(colorsArr)
       }) 
 }
-function renderColors(){
-  for( let i=0; i< colorsArr.length; i++){
-    document.querySelectorAll('.color-name').forEach(block=> {
-      return block.textContent = colorsArr[i++]
-    })
-  }
-  for(let i=0; i< colorsArr.length; i++){
+function renderColors(colorsArr){
+
+  document.getElementById('one').textContent = colorsArr[0]
+  document.getElementById('two').textContent = colorsArr[1]
+  document.getElementById('three').textContent = colorsArr[2]
+  document.getElementById('four').textContent = colorsArr[3]
+  document.getElementById('five').textContent = colorsArr[4]
+  let i =0
     document.querySelectorAll('.color-block').forEach(block => {
       return block.style.backgroundColor = colorsArr[i++]
     })
   }
-}
+
